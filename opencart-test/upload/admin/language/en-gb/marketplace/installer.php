2442a2b3faa8ef57ef66627acdbc0cbefa961a37
tcooksd@gmail.com <tcooksd@gmail.com>
2020-10-12 15:22:59 -0400

    Adding opencart to Jenkins repo

---
+++ opencart-test/upload/admin/language/en-gb/marketplace/installer.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']     = 'Extension Installer';
+
+// Text
+$_['text_upload']       = 'Requires a modification file with extension \'.ocmod.zip\'.';
+$_['text_success']      = 'Success: The extension has been installed!';
+$_['text_progress']     = 'Install Progress';
+$_['text_installed']    = 'Installed Extensions';
+
+// Column
+$_['column_image']      = 'Image';
+$_['column_name']       = 'Extension Name';
+$_['column_version']    = 'Version';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_progress']    = 'Progress';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify extensions!';
+$_['error_install']     = 'Install record could not be found!';
+$_['error_file']        = 'Install file %s could not be found!';
+$_['error_directory']   = 'Install directory %s could not be found!';
+$_['error_exists']      = 'Path %s already exists!';
+$_['error_unzip']       = 'Zip file could not be opened!';
+$_['error_upload']      = 'File could not be uploaded!';
+$_['error_filetype']    = 'Invalid file type!';
---
+++ opencart-test/upload/system/config/admin.php
@@ -0,0 +1,60 @@
+<?php
+// Site
+$_['site_url']          = HTTP_SERVER;
+
+// Database
+$_['db_autostart']      = true;
+$_['db_engine']         = DB_DRIVER; // mysqli, pdo or pgsql
+$_['db_hostname']       = DB_HOSTNAME;
+$_['db_username']       = DB_USERNAME;
+$_['db_password']       = DB_PASSWORD;
+$_['db_database']       = DB_DATABASE;
+$_['db_port']           = DB_PORT;
+
+// Session
+$_['session_autostart'] = false;
+$_['session_engine']    = 'db';
+
+// Error
+$_['error_display']     = true;
+
+// Actions
+$_['action_pre_action'] = array(
+	'startup/startup',
+	'startup/error',
+	'startup/event',
+	'startup/sass',
+	'startup/login',
+	'startup/permission'
+);
+
+// Actions
+$_['action_default']    = 'common/dashboard';
+
+// Action Events
+$_['action_event']      = array(
+	'controller/*/before' => array(
+		'event/language/before'
+	),
+	'controller/*/after' => array(
+		'event/language/after'
+	),
+	//'controller/*/before' => array(
+	//	'event/extension/controller'
+	//),
+	//'model/*/before' => array(
+	//	'event/extension/model'
+	//),
+	//'view/*/before' => array(
+		//	'event/extension/view'
+	//),
+	'view/*/before' => array(
+		999  => 'event/language'
+	),
+	//'model/*/after' => array(
+	//	'event/debug/before'
+	//),
+	//'model/*/after'  => array(
+	//	'event/debug/after'
+	//)
+);
